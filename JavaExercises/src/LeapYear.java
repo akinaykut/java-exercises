55564fcd60fd75113f35a998e16c32be64972b3a
akinaykut05 <akinaykut05@gmail.com>
2022-08-27 00:51:57 +0300

    Added LeapYear

---
+++ JavaExercises/src/LeapYear.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+
+public class LeapYear {
+
+    public static void main(String[] args) {
+        int date;
+
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.println("Welcome to the Leap Year Program...");
+
+        System.out.print("Please enter a year: ");
+        date = input.nextInt();
+
+
+        if(date % 4 == 0) {
+
+            if(date % 100 == 0 && date % 400 != 0) {
+                System.out.println("Not Leap");
+            }else {
+                System.out.println("Leap year");
+            }
+
+
+        }else {
+            System.out.println("Not Leap");
+        }
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
