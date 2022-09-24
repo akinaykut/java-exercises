9ecffaad8000cfb12cb9fd6d52b021e0c429f309
akinaykut05 <akinaykut05@gmail.com>
2022-09-24 23:24:03 +0300

    Initial commit of minefield

--- SalaryCalculator/src/Main.java
+++ SalaryCalculator/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -9,6 +11,34 @@ public class Main {
         ahmet.print();
 
 
+        int[] dizi1 = new int[10];
+
+        for(int i =0 ; i< 10; i++) {
+            dizi1[i]= i;
+            System.out.print(dizi1[i]);
+        }
+
+
+        System.out.println();
+
+        for( int i : dizi1) {
+            dizi1[i] = 5;
+            System.out.print(dizi1[i]);
+        }
+
+        int[][] dizi2 = new int[5][5];
+
+
+        System.out.println();
+        for(int[] u: dizi2) {
+            for(int i : u) {
+                u[i] = 0;
+                System.out.print(u[i]);
+            }
+            System.out.println();
+        }
+
+        System.out.println(Arrays.toString(dizi1));
     }
 
 }
